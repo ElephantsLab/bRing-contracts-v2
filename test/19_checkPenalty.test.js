20f8546a22704a51f95d8590f80cd4cd0267e077
OlyaKadynska <olga.kadinska@elephantslab.com>
2021-10-20 11:34:16 +0300

    add penalty tests & five users stake counting

--- test/19_checkPenalty.test.js
+++ test/19_checkPenalty.test.js
@@ -1,8 +1,6 @@
 const FirstToken = artifacts.require("FirstToken");
 const SecondToken = artifacts.require("SecondToken");
 
-const BRingToken = artifacts.require("bRingToken");
-
 const bRingFarmingContract = artifacts.require("BRingFarming");
 
 const {
@@ -13,17 +11,19 @@ const {
 } = require('@openzeppelin/test-helpers');
 
 contract("check penalty logic", async accounts => {
-    const [ deployer, firstAddr, secondAddr, thirdAddr, fourthAddr, fifthAddr, sixthAddr ] = accounts;
+    const [ deployer, firstAddr ] = accounts;
 
     let bRingFarming;
     let bRingFarmingAddress;
 
-    let firstToken, secondToken, BRNGToken;
-    let firstTokenAddress, secondTokenAddress, BRNGTokenAddress;
+    let firstToken, secondToken;
+    let firstTokenAddress, secondTokenAddress;
 
     let minStakeAmount = 1;
     let maxStakeAmount = 500000; // 500 000
     let totalStakeLimit = 1000000; // 1 000 000
+
+    let stakeAmount = 1000;
 
     beforeEach(async () => {
         // tokens deployed
@@ -32,9 +32,6 @@ contract("check penalty logic", async accounts => {
 
         secondToken = await SecondToken.new({ from: deployer });
         secondTokenAddress = secondToken.address;
-
-        BRNGToken = await BRingToken.new({ from: deployer });
-        BRNGTokenAddress = BRNGToken.address;
 
         // contract deployed
         bRingFarming = await bRingFarmingContract.new({ from: deployer });
@@ -108,6 +105,121 @@ contract("check penalty logic", async accounts => {
             0),
             'Invalid penalty receiver address'
         ); 
+    })
+
+    it("should not be penalty with maxPenalty zero", async () => {
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+        let tokenRewards = [1];
+        const rewardsTokenbits = (new BN(10)).pow(new BN(15));
+        const maxPenalty = new BN(0);
+        const penaltyDuration = 45 * 24 * 3600;
+
+        await bRingFarming.configPool(firstTokenAddress, (new BN(minStakeAmount)).mul(tokenbits), 
+            (new BN(maxStakeAmount)).mul(tokenbits), (new BN(totalStakeLimit)).mul(tokenbits),
+            [firstTokenAddress], 
+            [
+                (new BN(tokenRewards[0])).mul(rewardsTokenbits)
+            ],
+            maxPenalty, penaltyDuration, deployer,
+            constants.ZERO_ADDRESS,
+            0)
+
+        await firstToken.transfer(bRingFarmingAddress, (new BN(maxStakeAmount)).mul(tokenbits), { from: deployer });
+        let tokenContractBalance = await firstToken.balanceOf.call(bRingFarmingAddress);
+        assert.equal(tokenContractBalance.valueOf(), Number((new BN(maxStakeAmount)).mul(tokenbits)), `contract balance is wrong`);
+
+        await firstToken.transfer(firstAddr, (new BN(stakeAmount)).mul(tokenbits), { from: deployer });
+        let userBalance = await firstToken.balanceOf.call(firstAddr);
+        assert.equal(userBalance.valueOf(), Number((new BN(stakeAmount)).mul(tokenbits)), `user tokens balance is wrong`);
+
+        await firstToken.approve(bRingFarmingAddress, (new BN(stakeAmount)).mul(tokenbits), { from: firstAddr });
+        await bRingFarming.stake(firstAddr, firstTokenAddress, (new BN(stakeAmount)).mul(tokenbits), { from: firstAddr });
+
+        let stakeDetails = await bRingFarming.viewStakingDetails(firstAddr, { from: firstAddr });
+
+        assert.equal(stakeDetails[0].length, 1, `user number of stake is wrong`);
+        assert.equal(Number(stakeDetails[2]), Number((new BN(stakeAmount)).mul(tokenbits)), `user stake amount is wrong`);
+
+        let timesPassed = 1;
+        await time.increase(time.duration.hours(timesPassed));
+
+        let penaltyInfo = await bRingFarming.getPoolPenaltyInfo(firstTokenAddress, { from: firstAddr });
+        assert.equal(Number(penaltyInfo.penaltyPercent), 0, "penalty percent is wrong");
+
+        let userStakingDetails = await bRingFarming.viewStakingDetails(firstAddr, { from: firstAddr });
+        let stakeStartTime = Number(userStakingDetails[3][0]);
+
+        let poolData = await bRingFarming.pools(firstTokenAddress, { from: deployer });
+        let totalStaked = Number(poolData.totalStaked);
+        let rewRatePerTime = 0.001;
+        
+        let afterTimesPassed = stakeStartTime + (timesPassed * 3600);
+        let reward = stakeAmount * (( afterTimesPassed - stakeStartTime ) / totalStaked) * rewRatePerTime * 0.9;
+
+        let stakeId = stakeDetails[0][0];
+
+        await bRingFarming.claimReward(stakeId, { from: firstAddr });
+
+        let balance = Number(await firstToken.balanceOf.call(firstAddr, { from: firstAddr })) / tokenbits;
+        
+        assert.equal(balance.toFixed(2), (reward * tokenbits).toFixed(2), "user balance is wrong");
+    })
+
+    it("should not be penalty with penaltyDuration zero", async () => {
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+        let tokenRewards = [1];
+        const rewardsTokenbits = (new BN(10)).pow(new BN(15));
+        const maxPenalty = new BN(30);
+        const penaltyDuration = 0;
+
+        await bRingFarming.configPool(firstTokenAddress, (new BN(minStakeAmount)).mul(tokenbits), 
+            (new BN(maxStakeAmount)).mul(tokenbits), (new BN(totalStakeLimit)).mul(tokenbits),
+            [firstTokenAddress], 
+            [
+                (new BN(tokenRewards[0])).mul(rewardsTokenbits)
+            ],
+            maxPenalty, penaltyDuration, deployer,
+            constants.ZERO_ADDRESS,
+            0)
+
+        await firstToken.transfer(bRingFarmingAddress, (new BN(maxStakeAmount)).mul(tokenbits), { from: deployer });
+        let tokenContractBalance = await firstToken.balanceOf.call(bRingFarmingAddress);
+        assert.equal(tokenContractBalance.valueOf(), Number((new BN(maxStakeAmount)).mul(tokenbits)), `contract balance is wrong`);
+
+        await firstToken.transfer(firstAddr, (new BN(stakeAmount)).mul(tokenbits), { from: deployer });
+        let userBalance = await firstToken.balanceOf.call(firstAddr);
+        assert.equal(userBalance.valueOf(), Number((new BN(stakeAmount)).mul(tokenbits)), `user tokens balance is wrong`);
+
+        await firstToken.approve(bRingFarmingAddress, (new BN(stakeAmount)).mul(tokenbits), { from: firstAddr });
+        await bRingFarming.stake(firstAddr, firstTokenAddress, (new BN(stakeAmount)).mul(tokenbits), { from: firstAddr });
+
+        let stakeDetails = await bRingFarming.viewStakingDetails(firstAddr, { from: firstAddr });
+
+        assert.equal(stakeDetails[0].length, 1, `user number of stake is wrong`);
+        assert.equal(Number(stakeDetails[2]), Number((new BN(stakeAmount)).mul(tokenbits)), `user stake amount is wrong`);
+
+        let timesPassed = 1;
+        await time.increase(time.duration.hours(timesPassed));
+
+        let userStakingDetails = await bRingFarming.viewStakingDetails(firstAddr, { from: firstAddr });
+        let stakeStartTime = Number(userStakingDetails[3][0]);
+
+        let poolData = await bRingFarming.pools(firstTokenAddress, { from: deployer });
+        let totalStaked = Number(poolData.totalStaked);
+        let rewRatePerTime = 0.001;
+        
+        let afterTimesPassed = stakeStartTime + (timesPassed * 3600);
+        let reward = stakeAmount * (( afterTimesPassed - stakeStartTime ) / totalStaked) * rewRatePerTime * 0.9;
+
+        let stakeId = stakeDetails[0][0];
+
+        await bRingFarming.claimReward(stakeId, { from: firstAddr });
+
+        let balance = Number(await firstToken.balanceOf.call(firstAddr, { from: firstAddr })) / tokenbits;
+        
+        assert.equal(balance.toFixed(2), (reward * tokenbits).toFixed(2), "user balance is wrong");
     })
 
 })
---
+++ test/21_fiveUsersStakeCounting.test.js
@@ -0,0 +1,198 @@
+const FirstToken = artifacts.require("FirstToken");
+
+const bRingFarmingContract = artifacts.require("BRingFarming");
+
+const {
+    BN,
+    time,
+    constants
+} = require('@openzeppelin/test-helpers');
+
+contract("users make stake and claim before penalty duration time is up", async accounts => {
+    const [ deployer, firstAddr, secondAddr, thirdAddr, fourthAddr, fifthAddr ] = accounts;
+
+    let bRingFarming;
+    let bRingFarmingAddress;
+
+    let firstToken;
+    let firstTokenAddress;
+
+    let minStakeAmount = 1;
+    let maxStakeAmount = 500000; // 500 000
+    let totalStakeLimit = 1000000; // 1 000 000 
+
+    let stakeAmount = 10000;
+
+    let fourtyFiveDaysReward;
+
+    before(async () => {
+        // tokens deployed
+        firstToken = await FirstToken.new({ from: deployer });
+        firstTokenAddress = firstToken.address;
+
+        // contract deployed
+        bRingFarming = await bRingFarmingContract.new({ from: deployer });
+        bRingFarmingAddress = bRingFarming.address;
+    })
+
+    it("config Pool", async () => {
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+        let tokenRewards = [1];
+        const rewardsTokenbits = (new BN(10)).pow(new BN(15));
+        const maxPenalty = new BN(30);
+        const penaltyDuration = 45 * 24 * 3600;
+
+        await bRingFarming.configPool(firstTokenAddress, (new BN(minStakeAmount)).mul(tokenbits), 
+            (new BN(maxStakeAmount)).mul(tokenbits), (new BN(totalStakeLimit)).mul(tokenbits),
+            [firstTokenAddress], 
+            [
+                (new BN(tokenRewards[0])).mul(rewardsTokenbits)
+            ],
+            maxPenalty, penaltyDuration, deployer,
+            constants.ZERO_ADDRESS,
+            0)
+    })
+
+    it("send tokens to the contract address", async () => {
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+       
+        await firstToken.transfer(bRingFarmingAddress, (new BN(maxStakeAmount)).mul(tokenbits), { from: deployer });
+        let tokenContractBalance = await firstToken.balanceOf.call(bRingFarmingAddress);
+        assert.equal(tokenContractBalance.valueOf(), Number((new BN(maxStakeAmount)).mul(tokenbits)), `contract balance is wrong`);
+
+    })
+
+    it("users should have firstToken in their addresses", async () => {
+        let users = [firstAddr, secondAddr, thirdAddr, fourthAddr, fifthAddr];
+        let userBalance;
+
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+
+        for(let i = 0; i < users.length; i++) {
+            await firstToken.transfer(users[i], (new BN(stakeAmount)).mul(tokenbits), { from: deployer });
+            userBalance = await firstToken.balanceOf.call(users[i]);
+            assert.equal(userBalance.valueOf(), Number((new BN(stakeAmount)).mul(tokenbits)), `${users[i]} user tokens balance is wrong`);
+        }
+    })
+
+    it("users make stakes without referrer", async () => {
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+
+        let users = [firstAddr, secondAddr, thirdAddr, fourthAddr];
+        let stakeDetails;
+
+        for(let i = 0; i < users.length; i++) {          
+            await firstToken.approve(bRingFarmingAddress, (new BN(stakeAmount)).mul(tokenbits), { from: users[i] });
+            await bRingFarming.stake(users[i], firstTokenAddress, (new BN(stakeAmount)).mul(tokenbits), { from: users[i] });
+
+            stakeDetails = await bRingFarming.viewStakingDetails(users[i], { from: users[i] });
+
+            assert.equal(stakeDetails[0].length, 1, `${users[i]} user number of stake is wrong`);
+            assert.equal(Number(stakeDetails[2]), Number((new BN(stakeAmount)).mul(tokenbits)), `${users[i]} user stake amount is wrong`);
+        }
+    })
+
+    it("four users should get the same reward in 1 day", async () => {
+        await time.increase(time.duration.days(1));
+
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+
+        let users = [firstAddr, secondAddr, thirdAddr, fourthAddr];
+        let stakeDetails, stakeId, stakeRew, stakeReward;
+        for(let i = 0; i < users.length; i++) {
+            stakeDetails = await bRingFarming.viewStakingDetails(users[i], { from: users[i] });
+            stakeId = stakeDetails[0][0];
+
+            stakeRew = await bRingFarming.getStakeRewards(users[i], stakeId, false, { from: users[i] });
+            stakeReward = (Number(stakeRew[0]) * 0.9) / tokenbits;
+            console.log(`getStakeRewards ${i+1} user:`, stakeReward);
+        }
+       
+    })
+
+    it("four users should get the same reward in 44 day", async () => {
+        await time.increase(time.duration.days(43)); // 44 - 1 from previous test
+
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+
+        let users = [firstAddr, secondAddr, thirdAddr, fourthAddr];
+        let stakeDetails, stakeId, stakeRew, stakeReward;
+        
+        for(let i = 0; i < users.length; i++) {
+            stakeDetails = await bRingFarming.viewStakingDetails(users[i], { from: users[i] });
+            stakeId = stakeDetails[0][0];
+
+            stakeRew = await bRingFarming.getStakeRewards(users[i], stakeId, false, { from: users[i] });
+            stakeReward = (Number(stakeRew[0]) * 0.9) / tokenbits;
+            console.log(`getStakeRewards ${i+1} user:`, stakeReward);
+        }     
+    })
+
+    it("four users should get the same reward in 45 day", async () => {
+        let daysPassed = 45;
+        await time.increase(time.duration.days(daysPassed - 44)); // 45 - 44 from previous tests
+
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+
+        let users = [firstAddr, secondAddr, thirdAddr, fourthAddr];
+        let stakeDetails, stakeId, stakeRew, stakeReward;
+        
+        for(let i = 0; i < users.length; i++) {
+            stakeDetails = await bRingFarming.viewStakingDetails(users[i], { from: users[i] });
+            stakeId = stakeDetails[0][0];
+
+            stakeRew = await bRingFarming.getStakeRewards(users[i], stakeId, true, { from: users[i] });
+            stakeReward = (Number(stakeRew[0]) * 0.9) / tokenbits;
+        }
+        fourtyFiveDaysReward = stakeReward.toFixed(2); 
+    })
+
+    it("user makes stake without referrer", async () => {
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+        
+        await firstToken.approve(bRingFarmingAddress, (new BN(stakeAmount)).mul(tokenbits), { from: fifthAddr });
+        await bRingFarming.stake(fifthAddr, firstTokenAddress, (new BN(stakeAmount)).mul(tokenbits), { from: fifthAddr });
+
+        let stakeDetails = await bRingFarming.viewStakingDetails(fifthAddr, { from: fifthAddr });
+
+        assert.equal(stakeDetails[0].length, 1, `user number of stake is wrong`);
+        assert.equal(Number(stakeDetails[2]), Number((new BN(stakeAmount)).mul(tokenbits)), `user stake amount is wrong`);
+
+    })
+
+    it("five users should get the same reward in 90 day", async () => {
+        let daysPassed = 90;
+        await time.increase(time.duration.days(daysPassed - 45)); // 90 - 45
+        const decimals = await firstToken.decimals();
+        const tokenbits = (new BN(10)).pow(decimals);
+
+        let users = [firstAddr, secondAddr, thirdAddr, fourthAddr];
+        let stakeDetails, stakeId, stakeRew, stakeReward;
+
+        for(let i = 0; i < users.length; i++) {
+            stakeDetails = await bRingFarming.viewStakingDetails(users[i], { from: users[i] });
+            stakeId = stakeDetails[0][0];
+
+            stakeRew = await bRingFarming.getStakeRewards(users[i], stakeId, true, { from: users[i] });
+            stakeReward = (Number(stakeRew[0]) * 0.9) / tokenbits;
+        }
+
+        let fifthUserStakeDetails = await bRingFarming.viewStakingDetails(fifthAddr, { from: fifthAddr });
+        let fifthUserStakeId = fifthUserStakeDetails[0][0];
+
+        let fifthUserStakeRew = await bRingFarming.getStakeRewards(fifthAddr, fifthUserStakeId, true, { from: fifthAddr });
+        let fifthUserStakeReward = (Number(fifthUserStakeRew[0]) * 0.9) / tokenbits;
+
+        let rewWithFiveUsers = (stakeReward - Number(fourtyFiveDaysReward)).toFixed(2);
+        fifthUserStakeReward = fifthUserStakeReward.toFixed(2);
+        assert.equal(rewWithFiveUsers, fifthUserStakeReward, "reward for five users is not the same");
+    })
+})
